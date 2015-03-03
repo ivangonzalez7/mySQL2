cfb0a4027bec5844570d868971c140ec282f6151
Ivan <Ivan@10.189.164.107>
2015-03-03 14:44:18 +0100

    Mysql

---
+++ src/main/java/edu/upc/eetac/dsa/igonzalez/mysql2/Main.java
@@ -0,0 +1,10 @@
+package edu.upc.eetac.dsa.igonzalez.mysql2;
+
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    MySQLAccess dao = new MySQLAccess();
+    dao.readDataBase();
+  }
+
+} 
